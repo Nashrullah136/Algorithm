6f426fa8946eff075f85c29f110989fe7fe3dfc4
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2022-11-05 13:24:22 +0700

    fix logic and add test to on quicksort

--- sorting/quicksort.cpp
+++ sorting/quicksort.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
 #include <algorithm>
 using namespace std;
 
 int partition(int data[], int start, int end){
-    int pivot = (start-end)/2;
+    int pivot = start + (end-start)/2;
     int data_pivot = data[pivot];
     swap(data[pivot], data[end-1]);
-    int pos = start;
-    for (size_t i = start; i < end; i++){
-        if(data[i] < data_pivot){
-            swap(data[i], data[pos]);
-            pos++;
+    int left = start;
+    int right = end - 2;
+    do{
+        while(data[left] < data_pivot){
+            left++;
         }
-    }
-    swap(data[end-1], data[pos]);
-    return pos;
+        while(data[right] >= data_pivot){
+            right--;
+        }
+        if(left < right){
+            swap(data[left], data[right]);
+        }
+    } while(left < right);
+    swap(data[end-1], data[left]);
+    return left;
 }
 
 void quicksort(int data[], int start, int end){
-    if(start - end <= 1){
+    if(end - start <= 1){
         return;
     }
     else{
@@ -31,4 +41,18 @@ void quicksort(int data[], int start, int end){
 void quicksort(int data[], int length)
 {
     quicksort(data, 0, length);
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    int length = 35;
+    int data[35];
+    for (size_t i = 0; i < length; i++)
+    {
+        data[i] = rand() % length;
+    }
+    quicksort(data, length);
+    assert(is_sorted(begin(data), end(data)));
+    return 0;
 }
