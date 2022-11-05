fb1aa5137a02c33432d1e2cc81341208a8f933dd
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2022-11-05 12:27:42 +0700

    fix indexing on counting sort

--- sorting/countingsort.cpp
+++ sorting/countingsort.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
 using namespace std;
 
 void countingsort(int data[], int length, int min, int max)
 {
-    int *temp = new int[max - min]();
+    int range = max - min + 1;
+    int *temp = new int[range]();
     for (size_t i = 0; i < length; i++)
     {
         int temp_data = data[i] - min;
         temp[temp_data]++;
     }
     int now = 0;
-    for (size_t i = 0; i < max - min; i++)
+    for (size_t i = 0; i < range; i++)
     {
         for (size_t j = 0; j < temp[i]; j++)
         {
@@ -30,4 +35,18 @@ void countingsort(int data[], int length){
         mini = min(mini, data[i]);
     }
     countingsort(data, length, mini, maxi);
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    int length = 35;
+    int data[35];
+    for (size_t i = 0; i < length; i++)
+    {
+        data[i] = rand() % length;
+    }
+    countingsort(data, length);
+    assert(is_sorted(begin(data), end(data)));
+    return 0;
 }
