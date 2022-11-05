e1f103f7b969894a752ae737756567e0004dabb0
Nashrullah136 <muhammad.118140136@student.itera.ac.id>
2022-11-05 12:31:44 +0700

    add test to mergesort

--- sorting/mergesort.cpp
+++ sorting/mergesort.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
 using namespace std;
 
 void merge(int data[], int start, int mid, int end)
@@ -51,7 +55,7 @@ void mergesort(int data[], int start, int end)
     }
     else
     {
-        int mid = (end - start) / 2;
+        int mid = start + (end - start) / 2;
         mergesort(data, start, mid);
         mergesort(data, mid, end);
         merge(data, start, mid, end);
@@ -61,4 +65,18 @@ void mergesort(int data[], int start, int end)
 void mergesort(int data[], int length)
 {
     mergesort(data, 0, length);
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(NULL));
+    int length = 35;
+    int data[35];
+    for (size_t i = 0; i < length; i++)
+    {
+        data[i] = rand() % length;
+    }
+    mergesort(data, length);
+    assert(is_sorted(begin(data), end(data)));
+    return 0;
 }
